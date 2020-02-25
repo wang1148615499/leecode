9f285fe53f0554d5dbbb5ed376480776efbed66f
wangmingwei_test <1148615499@qq.com>
2020-02-25 20:40:37 +0800

    my first Cprogram

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月25日 星期二 20时39分27秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<string>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "ss" << endl;
+    return 0;
+}
