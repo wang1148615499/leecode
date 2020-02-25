02fa62c4e08e60e746a630c460684169a9613982
wangmingwei_test <1148615499@qq.com>
2020-02-25 20:30:40 +0800

    first test file

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月25日 星期二 20时29分19秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<string>
+using namespace std;
+
+int main() {
+
+    cout << "wangingwei" << endl;
+    return 0;
+}
