e60661bfccf1b45e94031b8f0693a94cef01d0e7
wangmingwei_test <1148615499@qq.com>
2020-02-27 17:38:57 +0800

    leetcode202.cpp

---
+++ leetcode202.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: leetcode202.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月27日 星期四 17时38分25秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<string>
+using namespace std;
+int get_next(int n) {
+    int temp = 0;
+    while (n) {
+        temp += (n % 10) * (n % 10);
+        n /= 10;
+    }
+    return temp;
+}
+
+bool isHappy(int n){
+    int p = n, q = n;
+    while (q != 1) {
+        p = get_next(p);
+        q = get_next(get_next(q));
+        if (p == q) break;
+    }
+    return q == 1;
+}
